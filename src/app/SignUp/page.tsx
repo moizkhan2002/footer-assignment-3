724670de86670c9c2d553be0b458ca87cf290ea4
moizkhan2002 <tariqjameel8234@gmail.com>
2024-11-01 12:11:13 -0700

    Add files via upload
    Assignment 3

---
+++ src/app/SignUp/page.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function SignUp(){
+    return( 
+    <div>
+   <h1 className="text-blue-900 text-bold text-2xl"> This Is My SignUp Page </h1>
+        </div>
+    )
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function about(){
+    return(
+        <div>
+   <h1 className="text-green-500 text-bold text-2xl">This Is My about Page</h1>
+        </div>
+    )
+}
